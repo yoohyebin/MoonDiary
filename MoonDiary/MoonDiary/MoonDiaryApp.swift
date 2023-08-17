b40e10be07af71d057f4e0937be20cc2892e7d59
Hyebin (Helia) <hyebin218@naver.com>
2023-08-17 20:33:56 +0900

    [Init] 초기 프로젝트 생성

---
+++ MoonDiary/MoonDiary/MoonDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonDiaryApp.swift
+//  MoonDiary
+//
+//  Created by hyebin on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct MoonDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
